bc5ea4db895a8ff6b96669607a937a29322193bd
mandara42 <jeje4247@yahoo.fr>
2022-11-11 06:16:38 +0100

    produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,33 +1,35 @@
-
+//url de la page
 function getURL(){
     return window.location.href
 }
+//id du produit
 function getID(urlPage){
     const url = new URL(urlPage);
     const searchParams = new URLSearchParams(url.search); 
     const id = searchParams.getAll("id") 
     return id
 }
-//recuperation du produit
-
 
 const actualURL = getURL()
 const idProduct = getID(actualURL)
-
+//ajout du produit
 fetch("http://localhost:3000/api/products/"+idProduct)
     .then (data => data.json())
     .then(jsonProduct => {
-        document.querySelector(".item__img").innerHTML = `<img src="${jsonProduct.imageUrl}" alt=${jsonProduct.altTxt}></img>`
+        document.querySelector(".item__img").innerHTML = `<img src="${jsonProduct.imageUrl}" alt="${jsonProduct.altTxt}"></img>`
         document.querySelector("#title").innerHTML = `${jsonProduct.name}`
         document.querySelector("#price").innerHTML = `${jsonProduct.price}`
         document.querySelector("#description").innerHTML = `${jsonProduct.description}`
-        document.querySelector("#colors").innerHTML = `<option value="vert">vert</option>
-        <option value="blanc">blanc</option>`
-        
-        return console.log(jsonProduct)
+
+        const colorTab = jsonProduct.colors.length
+        for ( let i = 0; i < colorTab; i++){
+            let newOption = document.createElement('option')
+            const valeur = jsonProduct.colors[i]
+            newOption.setAttribute("value", valeur)
+            newOption.innerHTML =valeur
+            document.querySelector("#colors").append(newOption)
+        }
+
+        return jsonProduct
         });
-
-
-console.log(actualURL)
-console.log(idProduct)
 
